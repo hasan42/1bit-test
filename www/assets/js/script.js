8cd1cf7a959513e41523872f9be2be5b5374575b
rpokosov <hasan83842@gmail.com>
2020-05-18 13:25:44 +0300

    add adaptive and other fixes

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -2,12 +2,13 @@
 
 $(document).ready(function () {
   var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
     spaceBetween: 30,
+    autoHeight: true,
     autoplay: {
       delay: 5000,
       disableOnInteraction: false,
     },
-    direction: 'vertical',
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
