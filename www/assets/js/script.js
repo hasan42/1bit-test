37e0a875d0569fc598f6d7c26088f90a2a5c8341
rpokosov <hasan83842@gmail.com>
2020-05-18 10:16:15 +0300

    add form validation

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -1,14 +1,57 @@
-window.onload = function () {
+"use strict";
+
+$(document).ready(function () {
   var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
-    // autoplay: {
-    //   delay: 3000,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
     direction: 'vertical',
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     }
   });
-};
+
+  function validation(event){
+
+    var validName = $("#name").val().length <= 0 ? false : true;
+    var validEmail = $("#email").val().length <= 0 ? false : true;
+
+    if( !validName ){
+      $("#name").next('.form-error').text("Введите имя");
+    }else{
+      $("#name").next('.form-error').text("");
+    }
+
+    if( !validEmail ){
+      $("#email").next('.form-error').text("Введите e-mail");
+    }else{
+      $("#email").next('.form-error').text("");
+    }
+
+    if( !validName || !validEmail ){
+      $("#submit").prop("disabled", true);
+    }else{
+      $("#submit").prop("disabled", false);
+    }
+  }
+
+  $("#submit").click(function(event){
+    event.preventDefault();
+    this.blur();
+
+    $("#submit").text("Загрузка ...");
+
+    setTimeout(()=>{
+      $("#form").hide();
+      $(".form-send").show();
+    },1500);
+
+  });
+
+  $("#name").on('blur keyup',validation);
+  $("#email").on('blur keyup',validation);
+
+});
